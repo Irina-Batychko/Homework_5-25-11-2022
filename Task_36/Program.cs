a1fb90b429aa134d108636a77aa3db320f57b412
IRINA BATYCHKO <mironenkoi@yandex.ru>
2022-11-30 02:03:22 +0300

    added tasks

---
+++ Task_36/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 36:
+Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0   */
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int sum = 0;
+
+FillArrayRandomNumbers(numbers, 1, 99);
+WriteArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i % 2 != 0) sum = sum + numbers[i];
+}
+Console.WriteLine($"Сумма элементов на нечетных позициях в массиве равна {sum}");
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Task_38/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 38:
+Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76  */
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+
+FillArrayRandomNumbers(numbers);
+WriteArray(numbers);
+
+double min = numbers[0];
+double max = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < min) min = numbers[i];
+    else if (numbers[i] > max) max = numbers[i];
+}
+Console.WriteLine($" Разница между максимальным и минимальным элементом массива {max - min}");
+
+void FillArrayRandomNumbers(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToDouble(new Random().Next(1000) / 10.0);
+    }
+}
+void WriteArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
